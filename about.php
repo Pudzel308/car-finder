442f1f128b8d06e2e1d6778a9532d8c4c7dbcfb9
Jase Karl <pudzel@archlinux.localdomain>
2025-05-30 11:55:32 +0000

    changes

---
+++ about.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+    <style>
+        <title>
+            body { font-family: Arial, sans-serif; padding: 20px; }
+        </title>
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+</html>
+<body>
+    <div class="m-5 p-5" style="border: rgb(255, 0, 0)"> 
+        <h1 class="text-lg">About</h1>
+        <hr>
+        <div>Jase Karl Rosales Janiola</div>
+        <div>Sundy Eupena Pasa</div>
+    </div>
+</body>
--- saved.php
+++ saved.php
@@ -4,30 +4,23 @@ $cars = $pdo->query("SELECT * FROM saved_cars")->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Saved Cars</title>
-  <style>
-    body { font-family: Arial, sans-serif; padding: 20px; }
-    button {
-      margin-top: 20px;
-      padding: 10px 20px;
-      font-size: 16px;
-      cursor: pointer;
-    }
-  </style>
-</head>
-<body>
-  <h1>Saved Cars</h1>
-  <ul>
-    <?php foreach ($cars as $car): ?>
-      <li><?= htmlspecialchars($car['make']) ?> <?= htmlspecialchars($car['model']) ?> (<?= $car['year'] ?>) - <?= $car['class'] ?></li>
-    <?php endforeach; ?>
-  </ul>
+    <head>
+        <title>Saved Cars</title>
+        <link rel="stylesheet" href="styles.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Saved Cars</h1>
+        <ul>
+            <?php foreach ($cars as $car): ?>
+            <li><?= htmlspecialchars($car['make']) ?> <?= htmlspecialchars($car['model']) ?> (<?= $car['year'] ?>) - <?= $car['class'] ?></li>
+<?php endforeach; ?>
+</ul>
 
-  <!-- Back to Search Button -->
-  <form action="index.html" method="get">
-    <button type="submit">Back to Search</button>
-  </form>
-</body>
+        <!-- Back to Search Button -->
+        <form action="index.html" method="get">
+            <button class="btn btn-outline-success" type="submit">Back to Search</button>
+        </form>
+    </body>
 </html>
 
